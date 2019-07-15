99960963eab7d1d23c3abdec2ac74484637f53b7
Sunny Beatteay <sbeatteay@digitalocean.com>
2019-07-15 10:57:37 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var childProcess = require('child_process');
+var githubUsername = 'sunny-b';
+var githubBranch = 'master';
+var deployPath = 'cd /home/sunny && ./deploy.sh';
+
+
+app.post("/webhooks/github", function (req, res) {
+    var sender = req.body.sender;
+    var branch = req.body.ref;
+
+    if (isProperTrigger(sender, branch)) {
+        deploy(res);
+    }
+})
+
+function isProperTrigger(sender, branch) {
+  return branch.indexOf(branch) > -1 && sender.login === username;
+}
+
+function deploy(res) {
+  childProcess.exec(deployPath, function(err, stdout, stderr) {
+    if (err) {
+      console.error(err);
+      return res.send(500);
+    }
+    res.send(200);
+  });
+}
