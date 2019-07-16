8fb5db3db68dfe8a881c9db1f416cf73a8eb0d61
Sunny Beatteay <sbeatteay@digitalocean.com>
2019-07-15 22:29:39 -0400

    add webhook secret"

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 var express = require("express");
+var crypto = require("crypto")
+var bcrypt = require("bcrypt")
+
 var app = express();
 var childProcess = require('child_process');
 var githubUsername = 'sunny-b';
 var githubBranch = 'master';
 var deployPath = 'cd /home/sunny && ./deploy.sh';
+var secret = process.env.WEBHOOK_SECRET;
 
 app.use(express.json());
 
 app.post("/webhooks/github", function (req, res) {
-    console.log("Received GitHub Webhook")
-    var sender = req.body.sender;
-    var branch = req.body.ref;
+  console.log("Received GitHub Webhook");
+  console.log(JSON.stringify(req.body));
 
-    if (isProperTrigger(sender, branch)) {
-        deploy(res);
-    }
+  if (!isProperTrigger(req)) {
+    console.error("invalid webhook");
+    return res.sendStatus(400);
+  }
+
+  console.log("Deploying new Docker instance");
+
+  deploy(res);
+
+  console.log("Webhook completed successfully");
 })
 
-function isProperTrigger(sender, branch) {
-  return branch.indexOf(githubBranch) > -1 && sender.login === githubUsername;
+function isProperTrigger(req) {
+  var sig = "sha1=" + crypto.createHmac('sha1', secret).update(JSON.stringify(req.body)).digest('hex');
+
+  return bcrypt.compareSync(req.headers['x-hub-signature'], sig);
 }
 
 function deploy(res) {
-  console.log("Deploying new Docker instance")
   childProcess.exec(deployPath, function(err, stdout, stderr) {
     if (err) {
       console.error(err);
-      return res.send(500);
+      return res.sendStatus(500);
     }
-    console.log("Webhook completed successfully")
-    res.send(200);
+
+    res.sendStatus(200);
   });
 }
 
